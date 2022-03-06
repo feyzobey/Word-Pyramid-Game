75f292c74848e0a4a5b0e78a4b523227fb5f7a7e
FaBeY34 <feyzullahasillioglu@gmail.com>
2022-03-06 18:52:47 +0300

    a

--- filter.py
+++ filter.py
@@ -8,7 +8,8 @@ with open("./wordlist.txt", "r") as f1, open("./filteredWords2.txt", "r") as f2:
     for line2 in f2:
         for word2 in line2.split():
             second.append(word2)
-    with open("./wordlist2.txt", "w") as f3:
+    
+with open("./wordlist2.txt", "w") as f3:
         for i in range(len(first)):
             for j in range(len(second)):
                 while j < len(first):
