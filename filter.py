12dea4c1fd847c505232dd44cb958dea6eb99111
FaBeY34 <feyzullahasillioglu@gmail.com>
2022-03-06 19:35:09 +0300

    a

--- filter.py
+++ filter.py
@@ -1,6 +1,5 @@
 first = []
 second = []
-diffWords = []
 with open("./wordlist.txt", "r") as f1, open("./filteredWords2.txt", "r") as f2:
     for line1 in f1:
         for word1 in line1.split():
@@ -8,11 +7,14 @@ with open("./wordlist.txt", "r") as f1, open("./filteredWords2.txt", "r") as f2:
     for line2 in f2:
         for word2 in line2.split():
             second.append(word2)
-    
 with open("./wordlist2.txt", "w") as f3:
     for i in range(len(first)):
         for j in range(len(second)):
-            while j < len(first):
+            j = 0
+            isDiff =  j < len(second)
+            while isDiff:
                 if first[i] == second[j]:
-                    break
-            f3.write(first[i])
+                    isDiff = False
+                j += 1
+            f3.write(first[i] + "\n")
+            break
